6b565f1d6f8cd45432459bf8d8c12007efadb124
Ciarra Marielle Altoveros <ciarraaltoveros@gmail.com>
2017-02-07 21:29:48 -0800

    deploy to heroku

--- src/server/config.js
+++ src/server/config.js
@@ -10,11 +10,11 @@ module.exports ={
         host: process.env.HOST || "localhost",
         riot_api_key: process.env.RIOT_API_KEY
     }
-    // ,
-    // production: {
-    //     rootPath: rootPath,
-    //     port: process.env.PORT || 80,
-    //     host: process.env.HOST || "https://powerful-harbor-68074.herokuapp.com/",
-    //     riot_api_key: process.env.RIOT_API_KEY
-    // }
+    ,
+    production: {
+        rootPath: rootPath,
+        port: process.env.PORT || 80,
+        host: process.env.HOST || "https://champselect.herokuapp.com/",
+        riot_api_key: process.env.RIOT_API_KEY
+    }
 }
